d3d09a81cd582063f244a08ec662a624e322073c
Stuart Alldritt <stuart.alldritt@rogers.com>
2014-02-16 12:25:24 -0500

    Improving the annotate script
    Made significant changes to the annotate script to make it easier to do
    batches of images.

--- annotate.m
+++ annotate.m
@@ -1,14 +1,31 @@
+function [] = annotate(min, max, picture_path)
+    for i=min:max
+        for j=0:1
+            output_postfix = 'red';
+            
+            if(j == 1)
+                output_postfix = 'blue';
+            end
+            
+            img_str = strcat('0', num2str(i));
 
-im = imread('IMG_0243.JPG'); 
-figure
-clf;
-h = imagesc(im);
-axis image
-
-[x, y] = ginput(12);
-m = [x, y];
-
- fileID = fopen('IMG_0243.txt','w');
- fprintf(fileID,'%6s %12s\n','x','y');
- fprintf(fileID,'%6.2f %12.8f\n',m);
- fclose(fileID);
+            im = imread(strcat(picture_path, 'IMG_', img_str, '.JPG')); 
+            figure
+            clf;
+            h = imagesc(im);
+            axis image
+            
+            title(output_postfix);
+            [x, y] = ginput();
+            m = [x, y];
+            close all;
+            
+            if(isempty(x) == false)
+                fileID = fopen(strcat(picture_path, 'IMG_', img_str, '_', output_postfix, '.txt'),'w');
+                fprintf(fileID,'%6s %12s\n','x','y');
+                fprintf(fileID,'%6.2f %12.8f\n',m);
+                fclose(fileID);
+            end            
+        end
+    end
+end
