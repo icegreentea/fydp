72bf5296fc8e08a6c52f98317f08bdcd2a99ade9
Cat <cmercerrun@gmail.com>
2014-02-19 06:36:26 -0500

    Removed results, added in test image use.

--- lane_detection.m
+++ lane_detection.m
@@ -1,8 +1,5 @@
+function lane_detection(raw_image, output)
 %% Perform lane detection on the given test image
-
-clear all;
-close all;
-clc;
 
 %% Initialization
 
@@ -10,7 +7,6 @@ clc;
 parameters
 
 %Load Test Image
-raw_image = imread('C:\Users\Cat\Documents\School\SYDE 4B\SYDE 462 - Systems Design Workshop\Test Images\FFS3NH.tif');
 gray_image = rgb2gray(raw_image);
 
 %Crop Image
@@ -27,24 +23,24 @@ gray_image_cropped = rgb2gray(raw_image_cropped);
 
 %Saliency - Static Thresholds
 [saliency_map_static, saliency_initial_lane_map_static] = saliency_lanes_static(raw_image_cropped, saliency_threshold_static, saliency_subwindows_static);
-figure; imshow(saliency_map_static, []); title('Static Saliency Map');
-figure; imshow(saliency_initial_lane_map_static, []); title('Static Threshold Saliency Mask');
+%figure; imshow(saliency_map_static, []); title('Static Saliency Map');
+%figure; imshow(saliency_initial_lane_map_static, []); title('Static Threshold Saliency Mask');
 
 %% Verify Mapping Using Clustering Check
 
 %Saliency - Static Threshold
 [saliency_verified_lane_map_static, s] = lane_verify(gray_image_cropped, gray_image, saliency_initial_lane_map_static, saliency_subwindows_static, fisher_threshold_saliency_static, minimum_pixel_count, intensity_threshold);
-figure; imshow(saliency_verified_lane_map_static, []); title('Static Threshold Saliency - Verified Lane Map');
+imwrite(saliency_verified_lane_map_static, strcat(output, '_.jpg'));
+%figure; imshow(saliency_verified_lane_map_static, []); title('Static Threshold Saliency - Verified Lane Map');
 
+lanes_out = raw_image;
 [i, j] = find(saliency_verified_lane_map_static==1);
-
-inv_skeleton = bwmorph(saliency_verified_lane_map_static, 'remove');
-figure; imshow(inv_skeleton, []); title('Static Threshold Saliency - Lane Edge Map');
-
-colour_mask = uint8(zeros(n_im_rows,n_im_cols,3));
-colour_mask(:,:,1) = uint8(saliency_verified_lane_map_static);
-lanes_out = raw_image.* colour_mask + raw_image;
-figure; imshow(lanes_out, []); title('Static Threshold Saliency - Verified Lanes');
-%imwrite(lanes_out, strcat('C:\Users\Cat\Documents\School\SYDE 4A\SYDE 461 - Systems Design Workshop\Mike Smart Code\Results\',imout, '.tif'));
+for x = 1:size(i)
+    lanes_out(i(x),j(x),1) = 255;
+    lanes_out(i(x),j(x),2) = 255;
+    lanes_out(i(x),j(x),3) = 0;
+end
+imwrite(lanes_out, strcat(output, '__.jpg'));
+%figure; imshow(lanes_out, []); title('Static Threshold Saliency - Verified Lanes');
 
 %line_detect(lanes_out);
